3c54ada421bc63beca446c2b2be503439ee8acb3
Michael Klein <miketheklein@gmail.com>
2018-07-29 14:31:44 -0700

    updated finished app

--- src/server/routes/dashboard.js
+++ src/server/routes/dashboard.js
@@ -1,27 +1,29 @@
 const apiClient = require('../../gateway/codeship');
 const router = require('express').Router();
 
+async function appendBuildsToProjects(orgId, project) {
+  project.builds = (await apiClient.getBuilds(orgId, project.uuid)).builds;
+  return project;
+}
 router.get('/', async (req, res) => {
-  async function appendBuildsToProjects(project) {
-    project.builds = (await apiClient.getBuilds(orgId, project.uuid)).builds;
-    return project;
-  }
-  
+
   // Authenticate the user
   const authorization = await apiClient.authenticate();
-  
+
   // Use their first organization UUID
   const orgId = authorization.organizations[0].uuid;
-  
+
   // Get the organization's projects
   let projects = (await apiClient.getProjects(orgId)).projects;
-  
+
   // Embed the latest builds into each project
-  projects = await Promise.all(projects.map(appendBuildsToProjects));
-  
+  projects = await Promise.all(projects.map((project) => {
+    return appendBuildsToProjects(orgId, project);
+  }));
+
   // Render the getProjects view with projects
   res.render('index', { projects });
-})
+});
 
 
 // async function getProjects(req, res) {
